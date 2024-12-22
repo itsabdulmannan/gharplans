ffb820301f90b05cc9df42d2a4c167c30116a5b5
abulmanan@devcotk.com <itsmannan56@gmail.com>
2024-12-22 14:43:56 +0500

    Config.json changed to config.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
+const sequelize = new Sequelize({
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
     logging: false,
-    timezone: '+00:00',
-})
+    timezone: '+00:00',  // UTC
+});
 
 sequelize.authenticate()
     .then(() => {
