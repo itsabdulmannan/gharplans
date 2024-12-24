6ffd6c4c863fa7a38710c308cc25f6d2b61f730b
abulmanan@devcotk.com <itsmannan56@gmail.com>
2024-12-24 21:47:10 +0500

    Vercel File Added

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,20 @@
 const { Sequelize } = require('sequelize');
 
+// const sequelize = new Sequelize({
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+//     logging: false,
+//     timezone: '+00:00',  // UTC
+// });
 const sequelize = new Sequelize({
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: false,
-    timezone: '+00:00',  // UTC
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 sequelize.authenticate()
